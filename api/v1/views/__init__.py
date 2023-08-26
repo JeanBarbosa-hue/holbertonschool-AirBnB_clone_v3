42cdd810ddefe1640feaf01a9bd9c4f7b748e336
Luis <luisayala323@gmail.com>
2023-08-26 17:48:23 -0400

    update init documentation

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Contains the API blueprint"""
+
+# Import the necessary modules
 from flask import Blueprint
 
-"""Create the blueprint instance"""
+# Create the blueprint instance
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
