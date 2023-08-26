0eb01018a388da1153e0bd9bc0acc05029d4ef44
Luis <luisayala323@gmail.com>
2023-08-26 15:56:04 -0400

    updates

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,34 +1,24 @@
 #!/usr/bin/python3
 """Server file"""
-
-import os
-from flask import Flask, jsonify
+from flask import Flask
 import models
-from models import storage
-from apps.v1.views import app_views
+from api.v1.views import app_views
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
 
 
-def create_app(config_name):
-    app = Flask(__name__)
-
-    app.config.from_object(config_name)
-
-    app.register_blueprint(app_views)
-
-    @app.teardown_appcontext
-    def close_db(error):
-        storage.close()
-
-    @app.errorhandler(404)
-    def not_found(error):
-        return jsonify({"error": "Not found"}), 404
-
-    return app
-
-    app = create_app(os.getenv('APP_CONFIG', 'default'))
+@app.teardown_appcontext
+def close_db(error):
+    storage.close()
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', 5000)),
+            threaded=True)
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,20 +1,8 @@
 #!/usr/bin/python3
 """Status route on app_views"""
 from api.v1.views import app_views
-from models import storage
 
 
-@app_views.route('/stats')
-def stats():
-    stats = {}
-    classes = {"Amenity": "amenities",
-               "City": "cities",
-               "Place": "places",
-               "Review": "reviews",
-               "State": "states",
-               "User": "users"}
-
-    for cls in classes:
-        stats[classes[cls]] = storage.count(cls)
-
-    return stats
+@app_views.route('/status')
+def status():
+    return {"status": "OK"}
