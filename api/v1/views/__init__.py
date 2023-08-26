fc7025d5e019c2747431161bd4719606dfe57467
Luis <luisayala323@gmail.com>
2023-08-26 18:42:28 -0400

    adding documentation + index_view import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,4 +6,6 @@ from flask import Blueprint
 # Create the blueprint instance
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
+# Import to include in blueprint
+from api.v1.views.index import *
 from api.v1.views.states import *
