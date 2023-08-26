68185b2eda5fa9f8650972fe131908c435f934ba
Luis <luisayala323@gmail.com>
2023-08-26 15:35:26 -0400

    changes  api/v1/views files

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Script to start the API"""
+"""Script for starting API server"""
 
-from models import storage
+
+from models.storage import storage
 from api.v1.views import app_views
 from os import environ
-from flask import Flask, jsonify, make_response
-from flask_cors import CORS
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
@@ -13,22 +13,24 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
-# Create a CORS instance with permissive settings (for demonstration purposes)
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
-
-
 @app.teardown_appcontext
-def close_db(exception):
+def close_db(error):
     """Close storage"""
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(exception):
+def not_found(error):
     """404 error"""
-    return make_response(jsonify({"error": "Not found"}), 404)
+    return jsonify(error="Not found"), 404
 
 
 if __name__ == "__main__":
     """Main"""
-    app.run(host='0.0.0.0', port=5000)
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, thread=True)
