407019726d0edb46c1337d94328d0403c3ee8222
Luis <luisayala323@gmail.com>
2023-08-26 17:33:11 -0400

    we finally got ask 4 to work!!!!!

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,5 +2,5 @@
 """Contains the API blueprint"""
 from flask import Blueprint
 
-# Create the blueprint instance
+"""Create the blueprint instance"""
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
