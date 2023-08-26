c97185048992a7a68ac7bd8e323f366525f9b89c
Luis <luisayala323@gmail.com>
2023-08-26 18:38:15 -0400

    fix init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Contains the API blueprint"""
 
-from api.v1.views.states import *
 from flask import Blueprint
 
 # Create the blueprint instance
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.states import *
